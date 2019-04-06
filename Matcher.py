d83023ce59d7ff18f92a6da4b67983256321b9ea
Rune Barrett <runebarrett@gmail.com>
2019-04-06 21:18:38 +0200

    accept abbreviations in input, remove all special characters without splitting a sentence on them, refactor+clean+comment

--- Matcher.py
+++ Matcher.py
@@ -8,69 +8,98 @@ import pickle
 import re
 
 USE_SAMPLE_DATA = True
-ORIG_BOOK_TEXT = open("data/text/008_7pet_sample_.txt",
-                      encoding="utf-8").read()
-#SAMPLE_ABBR = ["11. ", "bl.a."]
+ORIG_BOOK_TEXT = open("data/text/008_7pet_sample.txt", encoding="utf-8").read()
+ABBREVIATIONS = ["bl.a.", "ca.", "f.eks.", "d.", r"\d+\."]
+MATCH_DIGIT_WORD_REGEX = r"\d|\w"
+# SAMPLE_ABBR = ["11. ", "bl.a."]
 
 
 def main():
     """The main process of the program"""
 
-    # Collects data either through the cloud or locally
+    # Collect STT data either through the cloud or locally
+    transscribed_input = load_data()
 
-    response = load_data()
+    sentences = split_to_sentences(ORIG_BOOK_TEXT)
+    sentences = tokenize_and_clean_sentences(sentences)
 
-    sentences = split_and_clean_data()
-    sentences = remove_non_word_digit(sentences)
-
-    for sen in sentences:
-        print(sen, "\n")
-
-
-def matcher():
-    return None
+    # for sen in sentences:
+    #    print(sen, "\n")
 
 
-# TODO: Handle special characters that should remain in the sentence - removes period from "11. september" and similar
-def remove_non_word_digit(sentences):
-    sentences_corrected = []
+def tokenize_and_clean_sentences(sentences):
+    # for storing and returning the cleaned sentences
+    sentences_cleaned = []
+
     for s in sentences:
-        sentence = s.split()
-        sen_corrected = []
-        for w in sentence:
-            while(len(w) > 1 and re.search("\d|\w", w[0]) == None):
-                w = w[1:len(w)]
-            while(len(w) > 1 and re.search("\d|\w", w[len(w)-1]) == None):
-                w = w[:len(w)-1]
-            if(len(w) == 1):
-                if(re.search("\d|\w", w) == None):
-                    w = ""
-            sen_corrected.append(w)
-            #print(w, len(w))
-        print(" ".join(sen_corrected), "\n")
-        sentences_corrected.append(" ".join(sen_corrected))
-    return sentences_corrected
+        # for storing the cleaned words in a sentence
+        cleaned_tokens = []
+
+        # split sentence on " "
+        tokens = s.split()
+
+        for w in tokens:
+            # check if the word is a known abbreviation
+            found = False
+            for a in ABBREVIATIONS:
+                if (a == w):  # re.search(a, w)):
+                    found = True
+
+            # check patterns
+            if(re.search(r"\d+\.", w)):
+                found = True
+
+            # remove non word/digit characters from the start of the word
+            while(not found and len(w) >= 1 and re.search(MATCH_DIGIT_WORD_REGEX, w[0]) == None):
+                w = w[1:len(w)] if len(w) > 1 else ""
+
+            # if not an abbreviation, remove non word/digit characters from end of string
+            while(not found and len(w) >= 1 and re.search(MATCH_DIGIT_WORD_REGEX, w[len(w)-1]) == None):
+                w = w[:len(w)-1] if len(w) > 1 else ""
+
+            # if the word is only one character, check if word/digit
+            # if(len(w) == 1 and re.search(MATCH_DIGIT_WORD_REGEX, w) == None):
+            #    w = ""
+
+            # append the cleaned word to cleaned_tokens
+            cleaned_tokens.append(w)
+
+        # print(" ".join(cleaned_tokens), "\n")
+
+        # append the cleaned sentence to sentences_cleaned
+        sentences_cleaned.append(" ".join(cleaned_tokens))
+
+    return sentences_cleaned
     # print(s)
 
 
-def split_and_clean_data():
+def split_to_sentences(text_input):
     i = 0
     sentences = []
     sen = ""
-    for c in ORIG_BOOK_TEXT:
-        look_ahead_behind = ORIG_BOOK_TEXT[i-4:i+4]
-        sen = sen+c
-        if((c == "." or c == "?" or c == "!") and find_abbreviations(look_ahead_behind) == None):
+    for c in text_input:
+        look_ahead_behind = text_input[i-15:i+15]
+        if((c == "." or c == "?" or c == "!") and find_abbreviations(look_ahead_behind) == False):
             sentences.append(sen.strip())
             sen = ""
+        else:
+            sen = sen+c
         i += 1
     return sentences
 
 
 def find_abbreviations(text):
-    r = re.search("\d*\. [a-z|ø|å|æ]", text)
-    #print(r,  " - ", text, "\n")
-    return r
+    found = False
+
+    if(re.search(r"\d+\.", text)):  # re.search(r"\d+\. [a-z|ø|å|æ]", text)
+        found = True
+
+    for a in ABBREVIATIONS:
+        l = len(text)//2
+        text[l-len(a):l+len(a)]
+        if a in text:
+            found = True
+    return found
 
 
 def load_data():
